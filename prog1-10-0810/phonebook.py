a00d6e713652e5088c99b7ebe3ee2e6480711378
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-03-01 13:18:16 +0200

    Recent changes added

--- prog1-10-0810/phonebook.py
+++ prog1-10-0810/phonebook.py
@@ -31,6 +31,24 @@ def AddContact():
 
     contacts.append(contact)
 
+# UZDEVUMS
+# Izveido funkciju kas ļaus izdēst kontaktu
+# norādot kontakta vārdu (name)
+def DeleteContact():
+    name = input('Enter name: ')    
+    found = False
+
+    for contact in contacts:
+        if contact['name'] == name:
+            contacts.remove(contact)
+            found = True
+            break
+
+    if found:
+        print(f"Contact {name} has been deleted.")
+    else:
+        print(f"Contact {name} not found.")
+
 def SaveContacts():
     with open('prog1-10-0810/contacts.json', 'w', encoding='UTF8') as file:
         contacts_dict = {'contacts_list': contacts}
@@ -38,12 +56,15 @@ def SaveContacts():
 
 LoadContacts()
 while(True):
-    response = input('A-add new, P-print, E-exit: ')
+    response = input('A-add new, D-delete, P-print, E-exit: ')
     if response == 'A':        
         AddContact()
         SaveContacts()
     elif response == 'P':
         PrintContacts()
+    elif response == 'D':
+        DeleteContact()
+        SaveContacts()
     elif response == 'E':
         print('Bye bye!')
         break
