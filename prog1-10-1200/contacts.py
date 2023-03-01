ffbdeea1a1d6ea641ab05cb27c978c60b3abac43
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-03-01 14:15:31 +0200

    Contact deletion function is added

--- prog1-10-1200/contacts.py
+++ prog1-10-1200/contacts.py
@@ -25,6 +25,20 @@ def PrintContacts():
     for contact in contacts:
         print(f"{contact['name']}")
 
+# UDZDEVUMS
+# Izveido funkciju kas prasa no litotāja vārdu
+# izdēš kontaktu ar to vārdu
+
+def DeleteContact():
+    name = input('Enter contact name: ')
+    for contact in contacts:
+        if contact['name'] == name:
+            contacts.remove(contact)
+            print(f"Contact {name} is deleted.")
+            return
+    
+    print(f"Contact {name} was not found!")
+
 def SaveContacts():
     with open('prog1-10-1200/contacts.json', 'w', encoding='UTF8') as file:
         contacts_dict = {
@@ -42,10 +56,13 @@ def LoadContacts():
 
 contacts = LoadContacts()
 while(True):
-    response = input("A-add contact, P-print, E-exit: ")
+    response = input("A-add contact, D-delete,  P-print, E-exit: ")
     
     if response == 'A':
         AddContact()
+        SaveContacts()
+    elif response == 'D':
+        DeleteContact()
         SaveContacts()
     elif response == 'P':
         PrintContacts()           
